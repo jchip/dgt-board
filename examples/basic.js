bf2c68147d7a8acd07b748cc3c2c5c3de96281a3
Joel Chen <joel123@gmail.com>
2019-03-24 15:52:03 -0700

    maintain a simple verbatim of the dgt board

--- examples/basic.js
+++ examples/basic.js
@@ -1,23 +1,23 @@
-var DGT = require('..')
-var board = new DGT.Board('/dev/ttyUSB0')
+var DGT = require("..");
+var board = new DGT.Board("COM5");
 
-board.on('ready', function () {
-  console.log('Serial No:', board.serialNo)
-  console.log('Version:', board.versionNo)
-  console.log(board.chess.ascii())
-  console.log('-----')
-})
+board.on("ready", function() {
+  console.log("Serial No:", board.serialNo);
+  console.log("Version:", board.versionNo);
+  console.log(board._data.join(""));
+  console.log("-----");
+});
 
-board.on('data', function (data) {
-  console.log('Field:', data.field)
-  console.log('Piece:', data.piece)
-  console.log('-----')
-})
+board.on("data", function(data) {
+  console.log("Field:", data.field);
+  console.log("Piece:", data.piece);
+  console.log("-----");
+});
 
-board.on('move', function (move) {
-  console.log('Move:', move)
-  console.log('-----')
-})
+board.on("changed", function(data) {
+  console.log("changed:", data.join(""));
+  console.log("-----");
+});
 
 /**
  * TODO:
