3c46a2e2950486d9578247a21e0d5f9169fe200d
Joel Chen <joel123@gmail.com>
2019-03-24 22:43:05 -0700

    connect board to game using chess.js

--- examples/basic.js
+++ examples/basic.js
@@ -1,16 +1,45 @@
+var chess = require("chess.js");
+
 var DGT = require("..");
 var board = new DGT.Board("COM5");
+const protocol = require("../lib/protocol");
 
 var history = [];
 
 var backup;
 
+var game = new chess.Chess();
+
+const pieceByLetter = {
+  r: "ROOK",
+  n: "KNIGHT",
+  b: "BISHOP",
+  k: "KING",
+  q: "QUEEN",
+  p: "PAWN"
+};
+
 board.on("ready", function() {
   console.log("Serial No:", board.serialNo);
   console.log("Version:", board.versionNo);
-  console.log(board.ascii().join("\n"));
-  console.log("-----");
   backup = board.backup();
+  game.clear();
+  for (let i = 0; i < backup.length; i++) {
+    const p = backup[i];
+    if (p !== ".") {
+      const lp = p.toLowerCase();
+      game.put(
+        {
+          color: lp === p ? game.BLACK : game.WHITE,
+          type: game[pieceByLetter[lp]]
+        },
+        protocol.fields[i]
+      );
+    }
+  }
+  console.log(board.ascii().join("\n"));
+  console.log(game.ascii());
+  console.log("-----");
 });
 
 board.on("data", function(data) {
@@ -23,11 +52,19 @@ board.on("changed", function() {
   // console.log("changed:", data.join(""));
   console.log(board.ascii().join("\n"));
   console.log("-----");
-  const move = board.findMove("white", backup);
+  const move = board.findMove(game.turn() === game.WHITE ? "white" : "black", backup);
   if (move) {
-    console.log("move", move);
-    history.push(backup);
-    backup = board.backup();
+    const gameMove = game.move({
+      from: protocol.fields[move.from.index],
+      to: protocol.fields[move.to.index]
+    });
+    console.log("board move", move);
+    console.log("game move", gameMove);
+    if (gameMove) {
+      history.push(backup);
+      backup = board.backup();
+      console.log(game.ascii());
+    }
   }
 });
 
